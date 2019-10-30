8db34134cb5d84e419e89be5381d7b56a256f22f
Mauro Gauto <mauro.gauto@synapse.com>
2019-10-30 10:50:37 -0300

    start new branch

--- src/app/check-out/check-out.component.ts
+++ src/app/check-out/check-out.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { ShoppingCartItem } from '../shopping-cart/shopping-cartM/shopping-cart-item';
+import { CartService } from '../services/shopping-cart.service';
+import { ShoppingCart } from '../shopping-cart/shopping-cartM/shopping-cart';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-check-out',
   templateUrl: './check-out.component.html',
   styleUrls: ['./check-out.component.scss']
 })
-export class CheckOutComponent implements OnInit {
+export class CheckOutComponent implements OnInit { 
+  // shipping = {}; 
+  // cart: ShoppingCart;
+  // subscription: Subscription;
 
-  constructor() { }
+  // constructor(private cartService: CartService) {}
 
-  ngOnInit() {
+  async ngOnInit() {
+    // let cart$ = await this.cartService.getCart();
+    // this.subscription = cart$.subscribe(cart => this.cart = cart);
   }
+
+  // ngOnDestroy() {
+  //   this.subscription.unsubscribe();
+  // }
+
+  // placeOrder() {
+  //   let order = {
+  //     datePlaced: new Date().getDate(),
+  //     shipping: this.shipping,
+  //     items: this.cart.items.map(i => {
+  //       return {
+  //         product: {
+  //           title: i.title,
+  //           imageUrl: i.imageUrl,
+  //           price: i.price
+  //         },
+  //         quantity: i.quantity,
+  //         totalPrice: i.totalPrice
+  //       }
+  //     })
+  //   }
+  // }    
 
 }
